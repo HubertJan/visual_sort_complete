4469998f178260b008186b07e6bd2469fff7ea37
HubertJan <70658889+HubertJan@users.noreply.github.com>
2022-06-09 02:06:08 +0200

    feat: Add select dataSet dropdown, not functional yet

--- lib/screens/widgets/sort_visualisation/widgets/visualisation_side_bar.dart
+++ lib/screens/widgets/sort_visualisation/widgets/visualisation_side_bar.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pysort_flutter/providers/sort_graph_state.dart';
@@ -27,23 +28,80 @@ class VisualisationSideBar extends StatelessWidget {
             )
           ],
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        padding: EdgeInsets.all(16),
+        child: Stack(
           children: [
-            const SizedBox(
-              height: 16,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 8,
+                ),
+                Text("Datensatz"),
+                SizedBox(
+                  height: 8,
+                ),
+                DropdownButton2<String>(
+                  isExpanded: true,
+                  itemHeight: 32,
+                  dropdownDecoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(8)),
+                    color: HSLColor.fromColor(
+                            Theme.of(context).colorScheme.primary)
+                        .withLightness(0.3)
+                        .toColor(),
+                  ),
+                  buttonPadding:
+                      EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  buttonDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color:
+                        Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                  ),
+                  iconEnabledColor: Theme.of(context).colorScheme.onBackground,
+                  underline: const SizedBox(),
+                  buttonHeight: 32,
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        color: Colors.white,
+                      ),
+                  value: "100",
+                  items: const [
+                    DropdownMenuItem(
+                      child: Text("mit 100 Elementen"),
+                      value: "100",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("mit 200 Elementen"),
+                      value: "200",
+                    )
+                  ],
+                  onChanged: (newId) {
+                    /*   _currentDataSet =
+                    state.dataSets.firstWhere((element) => element.id == newId);
+                setState(() {}); */
+                  },
+                ),
+              ],
             ),
-            ProgresssBar(
-              currentValue: state.currentStepIndex,
-              maxValue: state.steps.length,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const SizedBox(
-              height: 64,
-              child: VisualisationControlButtons(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 16,
+                ),
+                ProgresssBar(
+                  currentValue: state.currentStepIndex,
+                  maxValue: state.steps.length,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SizedBox(
+                  height: 64,
+                  child: VisualisationControlButtons(),
+                ),
+              ],
             ),
           ],
         ),
